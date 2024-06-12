21f9e038b6e0e1baa968d639e0ff5d8152579d51
KaZZuSe <usemartin2@gmail.com>
2024-06-12 11:00:46 +0200

    ajustando deploy backend finish

--- backend/settings.py
+++ backend/settings.py
@@ -63,6 +63,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:5173",
      "http://127.0.0.1:5173",
+     "https://usa2-shop.vercel.app/"
  ]
 
 MEDIA_URL = '/media/'
